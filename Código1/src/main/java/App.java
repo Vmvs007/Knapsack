aa9dbec5e031d8e1ebdc54f8fd2d8ede1910ad6d
abiliomg <abilio_mg_castro@hotmail.com>
2019-06-13 15:27:15 +0100

    ola

--- Código1/src/main/java/App.java
+++ Código1/src/main/java/App.java
@@ -1,21 +1,128 @@
 public class App {
     public static void main(String[] args) {
-        int numOfItems=3;
-        int capacityOfKnapsack=5;
-
+        int numOfItems=12;
+        int capacityOfKnapsack=12;
+/*
         int[] weigthOfItems=new int[numOfItems+1];
-        weigthOfItems[1]=4;
-        weigthOfItems[2]=2;
-        weigthOfItems[3]=3;
+        weigthOfItems[1]=6;
+        weigthOfItems[2]=1;
+        weigthOfItems[3]=2;
+        weigthOfItems[4]=6;
+        weigthOfItems[5]=2;
+        weigthOfItems[6]=1;
+        weigthOfItems[7]=3;
+        weigthOfItems[8]=2;
+        weigthOfItems[9]=2;
+        weigthOfItems[10]=8;
+        weigthOfItems[11]=1;
+        weigthOfItems[12]=2;
+        weigthOfItems[13]=1;
+        weigthOfItems[14]=1;
+        weigthOfItems[15]=6;
+        weigthOfItems[16]=2;
+        weigthOfItems[17]=3;
+        weigthOfItems[18]=2;
+        weigthOfItems[19]=3;
+        weigthOfItems[20]=1;
 
         int[] profitOfItems=new int[numOfItems+1];
-        profitOfItems[1]=10;
-        profitOfItems[2]=4;
-        profitOfItems[3]=7;
+        profitOfItems[1]=45;
+        profitOfItems[2]=20;
+        profitOfItems[3]=8;
+        profitOfItems[4]=43;
+        profitOfItems[5]=10;
+        profitOfItems[6]=10;
+        profitOfItems[7]=24;
+        profitOfItems[8]=18;
+        profitOfItems[9]=15;
+        profitOfItems[10]=30;
+        profitOfItems[11]=50;
+        profitOfItems[12]=32;
+        profitOfItems[13]=11;
+        profitOfItems[14]=11;
+        profitOfItems[15]=49;
+        profitOfItems[16]=12;
+        profitOfItems[17]=28;
+        profitOfItems[18]=16;
+        profitOfItems[19]=25;
+        profitOfItems[20]=19;
+*/
+ /*int[] weigthOfItems=new int[numOfItems+1];
+        weigthOfItems[1]=1;
+        weigthOfItems[2]=1;
+        weigthOfItems[3]=3;
+        weigthOfItems[4]=1;
+        weigthOfItems[5]=4;
+        weigthOfItems[6]=4;
+        weigthOfItems[7]=1;
+        weigthOfItems[8]=2;
+        weigthOfItems[9]=1;
+        weigthOfItems[10]=1;
+        weigthOfItems[11]=2;
+        weigthOfItems[12]=5;
 
+        int[] profitOfItems=new int[numOfItems+1];
+        profitOfItems[1]=15;
+        profitOfItems[2]=45;
+        profitOfItems[3]=10;
+        profitOfItems[4]=8;
+        profitOfItems[5]=10;
+        profitOfItems[6]=30;
+        profitOfItems[7]=25;
+        profitOfItems[8]=12;
+        profitOfItems[9]=35;
+        profitOfItems[10]=50;
+        profitOfItems[11]=40;
+        profitOfItems[12]=29;
+*/
+ /*
+ int[] weigthOfItems=new int[numOfItems+1];
+        weigthOfItems[1]=6;
+        weigthOfItems[2]=1;
+        weigthOfItems[3]=2;
+        weigthOfItems[4]=6;
+        weigthOfItems[5]=2;
+        weigthOfItems[6]=1;
+
+        int[] profitOfItems=new int[numOfItems+1];
+        profitOfItems[1]=45;
+        profitOfItems[2]=20;
+        profitOfItems[3]=8;
+        profitOfItems[4]=43;
+        profitOfItems[5]=10;
+        profitOfItems[6]=10;
+*/
+ int[] profitOfItems=new int[numOfItems+1];
+        profitOfItems[1]=45;
+        profitOfItems[2]=20;
+        profitOfItems[3]=8;
+        profitOfItems[4]=43;
+        profitOfItems[5]=10;
+        profitOfItems[6]=10;
+        profitOfItems[7]=24;
+        profitOfItems[8]=18;
+        profitOfItems[9]=15;
+        profitOfItems[10]=30;
+        profitOfItems[11]=50;
+        profitOfItems[12]=32;
+ int[] weigthOfItems=new int[numOfItems+1];
+        weigthOfItems[1]=6;
+        weigthOfItems[2]=1;
+        weigthOfItems[3]=2;
+        weigthOfItems[4]=6;
+        weigthOfItems[5]=2;
+        weigthOfItems[6]=1;
+        weigthOfItems[7]=3;
+        weigthOfItems[8]=2;
+        weigthOfItems[9]=2;
+        weigthOfItems[10]=8;
+        weigthOfItems[11]=1;
+        weigthOfItems[12]=2;
         Knapsack knapsack=new Knapsack(numOfItems,capacityOfKnapsack,weigthOfItems,profitOfItems);
         knapsack.solve();
         knapsack.showResult();
+knapsack.showTable();
+        System.out.println("Quantidade de iterações: " + knapsack.getCount());
     }
 
 }
--- Código1/src/main/java/Knapsack.java
+++ Código1/src/main/java/Knapsack.java
@@ -1,10 +1,12 @@
 public class Knapsack {
+
     private int numOfItems;
     private int capacityOfKnapsack;
     private int[][] knapsackTable;
     private int totalBenefit;
     private int[] weights;
     private int[] values;
+    private int count=0;
 
     public Knapsack(int numOfItems, int capacityOfKnapsack, int[] weights, int[] values) {
         this.numOfItems = numOfItems;
@@ -15,20 +17,25 @@ public class Knapsack {
     }
 
     public void solve(){
-        //time complexity: O(N*W)
         for(int i=1;i<numOfItems+1;i++){
+            count++;
             for(int w=1;w<capacityOfKnapsack+1;w++){
-                int notTakingItem=knapsackTable[i-1][w];//not taking item i
+                count++;
+                int notTakingItem=knapsackTable[i-1][w];
                 int takingItem=0;
 
                 if(weights[i]<=w){
+
                     takingItem=values[i]+knapsackTable[i-1][w-weights[i]];
 
                 }
+                count++;
 
                 knapsackTable[i][w]=Math.max(notTakingItem,takingItem);
             }
+            count++;
         }
+        count++;
         totalBenefit=knapsackTable[numOfItems][capacityOfKnapsack];
 
     }
@@ -37,10 +44,30 @@ public class Knapsack {
         System.out.println("Total benefit: " + totalBenefit);
 
         for(int n=numOfItems,w=capacityOfKnapsack;n>0;n--){
+            //count++;
             if(knapsackTable[n][w]!=0 && knapsackTable[n][w]!=knapsackTable[n-1][w]){
+               // count+=2;
                 System.out.println("We take item: #"+n);
                 w=w-weights[n];
+            }else if(knapsackTable[n][w]==0){
+                //count++;
+            } else if (knapsackTable[n][w]!=0 && knapsackTable[n][w]==knapsackTable[n-1][w]) {
+                //count+=2;
             }
         }
     }
+    public void showTable(){
+        for(int n=0;n<numOfItems+1;n++){
+            for(int j=0;j<capacityOfKnapsack+1;j++){
+                System.out.print("|"+knapsackTable[n][j]+"|");
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+    }
+
+    public int getCount(){
+        return this.count;
+    }
+
 }
